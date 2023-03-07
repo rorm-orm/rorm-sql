497dda9cd6df90edff908d1cc6c76bc26a4151ad
gammelalf <gammelalf@hopfen.space>
2023-03-07 21:28:08 +0100

    Removed thiserror dependency

--- src/error.rs
+++ src/error.rs
@@ -1,9 +1,20 @@
-/**
-Error type to simplify propagating different error types.
- */
-#[derive(Debug, thiserror::Error)]
+/// Error type to simplify propagating different error types.
+use std::{error, fmt};
+
+#[derive(Debug)]
 pub enum Error {
     /// Error while building sql.
-    #[error("sql build error: {0}")]
-    SQLBuildError(String)
+    SQLBuildError(String),
+}
+
+impl error::Error for Error {}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Error::SQLBuildError(error) => {
+                write!(f, "sql build error: {error}")
+            }
+        }
+    }
 }
